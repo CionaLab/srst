73e73d69ec5c85421c5e92624150ee36d23f133f
Yishen Miao <mys721tx@gmail.com>
2020-05-10 12:00:28 -0700

    Removing space after unrary ~
    Removes space after the unarary ~ operator.

--- diff_expr.R
+++ diff_expr.R
@@ -28,19 +28,19 @@ exprs <- read_tsv("data/expression_matrix_10stage.tsv")
 
 meta_cell <- read_tsv("data/ciona10stage.cluster.upload.new.txt") %>%
 rename_all(tolower) %>%
-rename_all(~ sub(" ", "_", .)) %>%
+rename_all(~sub(" ", "_", .)) %>%
 separate(name, c("stage", "barcode"), "_") %>%
 separate(stage, c("stage", "replica"), "\\.")
 
 mat_exprs <- FromMatrix(as_matrix(exprs), meta_cell)
 
 mat_subsets <- split(mat_exprs, "tissue_type") %>%
-future_map(~ split(.x, "stage"))
+future_map(~Split(.x, "stage"))
 
 comparisons <- future_map2(
     head(names(stages), -1),
     tail(names(stages), -1),
-    ~ c(.x, .y)
+    ~c(.x, .y)
 )
 
 options(future.globals.maxSize =  16 * 1024 * 1024 * 1024)
@@ -49,42 +49,42 @@ mat_subsets <- future_map(
     mat_subsets,
     function(subset) future_map(
         comparisons,
-        ~ future_map(.x, ~ subset[stages[[.x]]])
+        ~future_map(.x, ~subset[stages[[.x]]])
     ) %>%
-    future_map(~ future_pmap(.x, cbind)) %>%
+    future_map(~future_pmap(.x, cbind)) %>%
     unlist()
 )
 
 # reset memory limit to avoid running out of memory.
 options(future.globals.maxSize = 500 * 1024 * 1024)
 
-zlm_output <- map(mat_subsets, ~ map(.x, ~zlm(~stage, .x)))
+zlm_output <- map(mat_subsets, ~map(.x, ~zlm(~stage, .x)))
 
-sumr <- map(zlm_output, ~ map(.x, ~ summary(.x, doLRT = TRUE)))
+sumr <- map(zlm_output, ~map(.x, ~summary(.x, doLRT = TRUE)))
 
 fc <- map(
     sumr,
-    ~ map(
+    ~map(
         .x,
-        ~ .x$datatable[contrast != "(Intercept)"]
+        ~.x$datatable[contrast != "(Intercept)"]
     ) %>%
     map(
-        ~ merge(
+        ~merge(
             .x[component == "H", .(primerid, `Pr(>Chisq)`)],
             .x[component == "logFC", .(primerid, coef, ci.hi, ci.lo)],
             by = "primerid"
         )
     ) %>%
     map(
-        ~ .x[, fdr := p.adjust(`Pr(>Chisq)`, "fdr")]
+        ~.x[, fdr := p.adjust(`Pr(>Chisq)`, "fdr")]
     )
 )
 
 fc_filtered <- map(
     fc,
-    ~ map(
+    ~map(
         .x,
-        ~ as_tibble(.x) %>%
+        ~as_tibble(.x) %>%
         filter(fdr < 0.05 & abs(coef) > log2(1.5)) %>%
         rename(`Pr(>Chisq)` = "chisq") %>%
         arrange(fdr, abs(coef))
@@ -92,24 +92,24 @@ fc_filtered <- map(
 )
 
 tissue_type <- names(fc_filtered) %>%
-map(~ sub("\\W+", "_", .x)) %>%
+map(~sub("\\W+", "_", .x)) %>%
 unlist()
 
 map(
     fc_filtered,
-    ~ map2(
+    ~map2(
         .x,
         map(
             comparisons,
             ~paste(.x, collapse = "-")
         ),
-        ~ add_column(.x, comp = .y)
+        ~add_column(.x, comp = .y)
     ) %>%
     bind_rows()
 ) %>%
 map2(
     tissue_type,
-    ~ add_column(.x, tissue_type = .y)
+    ~add_column(.x, tissue_type = .y)
 ) %>%
 bind_rows() %>%
 write_tsv("diff_expr.tsv")
